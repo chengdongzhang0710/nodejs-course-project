c4a75e87e3df193afcad3e52634afc8ee7e02920
chengdongzhang0710 <chengdongzhang0710@outlook.com>
2021-01-10 13:06:53 -0500

    Setup a ssl server

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const https = require('https');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -28,6 +29,9 @@ const store = new MongoDBStore({
     collection: 'sessions',
 });
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -126,7 +130,9 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(MONGODB_URI)
     .then(() => {
-        app.listen(process.env.PORT || 3000);
+        https
+            .createServer({ key: privateKey, cert: certificate }, app)
+            .listen(process.env.PORT || 3000);
     })
     .catch((err) => {
         console.log(err);
